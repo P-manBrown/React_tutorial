d5096e1060921c7f09af68e3698249f61d1156fe
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-01-05 15:40:58 +0900

    [Finish] modernJS-practice1-2

--- jsToReact/src/index.js
+++ jsToReact/src/index.js
@@ -1,9 +1,74 @@
+// const,let等の変数宣言
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// 以前はvarが使用されていた
+var val1 = "var変数";
+console.log(val1);
+
+// var変数は上書き可能
+val1 = "var変数を上書き";
+console.log(val1);
+
+// var変数は再宣言可能
+var val1 = "var変数を再宣言";
+console.log(val1);
+
+
+// var変数は意図しない上書きや再宣言が発生する可能性があるため、以下を使用するようになった
+
+// let変数
+let val2 = "let変数";
+console.log(val2);
+
+// let変数は上書き可能
+val2 = "let変数を上書き";
+console.log(val2);
+
+// letは再宣言不可
+// src/index.js:25:4: Identifier 'val2' has already been declared. (25:4)のエラー発生
+// let val2 = "let変数を再宣言"
+
+// const変数
+const val3 = "const変数";
+console.log(val3);
+// const変数は上書き不可
+// TypeError: "val3" is read-onlyのエラー発生
+// val3 = "const変数上書き";
+
+// const変数は再宣言不可
+// src/index.js:39:6: Identifier 'val3' has already been declared. (39:6)のエラー発生
+// const val3 = "const変数を再宣言"
+
+// constで定義したオブジェクトの要素は更新することができる
+const val4 = {
+  name: "田中",
+  age: 30,
+};
+// console.log(val4);
+// 変更
+val4.name = "Tanaka";
+// 追加
+val4.addres = "東京都";
+console.log(val4);
+
+// 変数宣言はconstを使用することがほとんど
+
+// constで定義した配列についても要素を変更することができる
+const val5 = [ 'dog', 'cat' ];
+// 変更
+val5[0] = "犬";
+// 追加
+val5.push(`bird`);
+console.log(val5);
+
+
+// テンプレート文字列
+const name = "田中";
+const age = 30;
+// 「私の名前は田中です。年齢は30歳です。」を出力したい
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
